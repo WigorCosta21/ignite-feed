3bf0d116e1918e4ce2ff0cea27ff1f55fef98dc1
wigor <wigor21@hotmail.com>
2024-11-10 13:43:45 -0300

    feat: created sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { Header } from "./components/Header";
 import "./global.css";
+import styles from "./App.module.css";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>Posts</main>
+      </div>
     </>
   );
 };
