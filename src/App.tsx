d4a17b46602b7a346097a1ce2ecf929e715f4ae0
wigor <wigor21@hotmail.com>
2024-11-10 19:34:23 -0300

    feat: created post component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Header } from "./components/Header";
-import "./global.css";
-import styles from "./App.module.css";
+import Post from "./components/Post";
 import Sidebar from "./components/Sidebar";
+
+import styles from "./App.module.css";
+import "./global.css";
 
 const App = () => {
   return (
@@ -10,7 +12,10 @@ const App = () => {
 
       <div className={styles.wrapper}>
         <Sidebar />
-        <main>Posts</main>
+        <main>
+          <Post />
+          <Post />
+        </main>
       </div>
     </>
   );
---
+++ src/components/Post.tsx
@@ -0,0 +1,52 @@
+import styles from "./Post.module.css";
+
+const Post = () => {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/106445848?v=4"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Wigor Ribeiro</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="10 de Novembro às 18:48h" dateTime="2024-11-10 18:48:10">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀{" "}
+        </p>
+
+        <p>
+          <a href="#">WigorCosta21</a>
+        </p>
+
+        <p>
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
+  );
+};
+
+export default Post;
